fa6b46e40b436bb79380cbaad3e26db5ec310f50
Aluno <>
2019-05-17 21:06:05 -0300

    Criação das classes e atualização da pagina admin

--- trabalho/models.py
+++ trabalho/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+#toda classe no django é um model
+class Estado(models.Model):
+    #nome_do_atributo = models.Tipo(configuração)
+    sigla   = models.CharField(max_length=2)
+    nome    = models.CharField(max_length=50)
+    
+    #Como se fosse toString e self = this
+    def __str__(self):
+        return self.sigla + ' - ' + self.nome
+
+
+class Cidade(models.Model):
+    #Qunado você tem uma palavra toda em maiuscula significa que ela é uma constante
+    nome        = models.CharField(max_length=50)
+    estado      = models.ForeignKey(Estado, on_delete=models.PROTECT) 
+    descricao   = models.TextField(
+        blank=True, 
+        null=True, 
+        verbose_name="Descrição", 
+        help_text="Espaço para colocar qualquer informação."
+        )
+
+    def __str__(self):
+        return self.nome + " - " + self.estado.sigla
+
+
+class Pessoa(models.Model):
+    nome        = models.CharField(max_length=100, help_text="Digite seu nome completo")
+    nascimento  = models.DateField(verbose_name="Data de nascimento")
+    email       = models.EmailField(max_length=100, help_text="Digite seu e-mail")
+    cidade      = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome + ' - ' + str(self.nascimento)

252814e1287f30ddb49171ec71c2a0f025ee56b1
Aluno <>
2019-07-05 22:35:35 -0300

    criação do AveCalc

---
+++ TrabalhoDjango2/trabalho/urls.py
@@ -0,0 +1,38 @@
+# Módulo do Django para criar urls
+from django.urls import path
+
+# Importe todas suas classes do views.py
+from .views import *
+
+urlpatterns = [
+    # path(
+    #   'caminho/da/url',
+    #   ClasseLáDoView.as_view(),
+    #    name='nomeDessaUrl')
+    path('', PaginaInicialView.as_view(), name="index"),
+    path('sobre/', SobreView.as_view(), name="sobre"),
+    path('contato/', ContatoView.as_view(), name="contato"),
+    path('cadastros/', CadastrosView.as_view(), name="cadastros"),
+    path('curriculo/', CurriculoView.as_view(), name="curriculo"),
+
+    #URLS de cadastros
+    path('cadastrar/estado/', EstadoCreate.as_view(), name="cadastrar-estado"),
+    path('cadastrar/cidade/', CidadeCreate.as_view(), name="cadastrar-cidade"),
+    path('cadastrar/pessoa/', PessoaCreate.as_view(), name="cadastrar-pessoa"),
+
+    #URLS de alterar cadastros
+    path('editar/estado/<int:pk>/', EstadoUpdate.as_view(), name="editar-estado"),
+    path('editar/cidade/<int:pk>/', CidadeUpdate.as_view(), name="editar-cidade"),
+    path('editar/pessoa/<int:pk>/', PessoaUpdate.as_view(), name="editar-pessoa"),
+
+    #URLS de excluir cadastros
+    path('excluir/estado/<int:pk>/', EstadoDelete.as_view(), name="excluir-estado"),
+    path('excluir/cidade/<int:pk>/', CidadeDelete.as_view(), name="excluir-cidade"),
+    path('excluir/pessoa/<int:pk>/', PessoaDelete.as_view(), name="excluir-pessoa"),
+
+    #URLS de excluir cadastros
+        path('listar/estado/', EstadoList.as_view(), name="listar-estados"),
+
+
+    
+]
